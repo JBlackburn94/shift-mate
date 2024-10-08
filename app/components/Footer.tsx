fa68e48691f4bdeaa40416d8864f158d80265dcd
Jason Blackburn <blackburnjason1994@gmail.com>
2024-10-08 19:24:04 +0100

    adds larger font size to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,26 +2,26 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col md:flex-row gap-10 justify-center md:justify-between items-start h-[500px] md:h-[200px] w-full bg-sm-blue text-white p-5 mt-10 mx-auto">
+    <footer className="flex flex-col md:flex-row justify-center md:justify-between items-start h-[500px] md:h-[200px] w-full bg-sm-blue text-white p-5 mt-10 mx-auto">
       <div className="w-full md:w-1/2 flex flex-col justify-start items-center">
-        <h4 className="font-semibold text-2xl">Contact</h4>
-        <p className="mt-2">info@shiftmate.co.uk</p>
-        <p>Contact number</p>
+        <h4 className="font-semibold text-3xl">Contact</h4>
+        <p className="mt-2 text-xl">info@shiftmate.co.uk</p>
+        <p className=" text-xl">Contact number</p>
       </div>
-      <div className="w-full md:w-1/2 flex flex-col justify-start items-center">
-        <Link href="#" className="mt-2 underline">
+      <div className="w-full md:w-1/2 flex flex-col justify-start items-center mt-2 md:mt-0">
+        <Link href="#" className="text-xl underline">
           WhatsApp
         </Link>
-        <Link href="#" className="underline">
+        <Link href="#" className="text-xl underline">
           Facebook
         </Link>
-        <Link href="#" className="underline">
+        <Link href="#" className="text-xl underline">
           Instagram
         </Link>
-        <Link href="#" className="underline">
+        <Link href="#" className="text-xl underline">
           Instagram
         </Link>
-        <Link href="#" className="underline">
+        <Link href="#" className="text-xl underline">
           Xiaohongshu
         </Link>
       </div>
